7c5a2a05121c42eec596785c840cecc8236d6667
LordRegis22 <45404541+LordRegis22@users.noreply.github.com>
2020-10-10 11:26:33 -0700

    remove unnecessary console log

--- server/analyticsRouter.js
+++ server/analyticsRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 function analyticsRouter(traql) {
-  console.log('in AnalyticsRouter');
+
   return router.post(
     '/',
     (req, res, next) => {
