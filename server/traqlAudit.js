bfc89da7d4d8758de3260603323b20738f503b13
jpinchak <jpinchak@gmail.com>
2020-10-16 00:17:38 -0700

    continue refactoring of moving functionality to aql-monitor

--- server/traqlAudit.js
+++ server/traqlAudit.js
@@ -1,64 +1,48 @@
 const axios = require('axios');
 
 function traqlAudit(traql) {
+  let aqlsToBeSent = {
+    successAqls: [],
+    errorAqls: []
+  }
   let open = true;
   if(open) {
     open = false;
-    // if there are any mutations that have analytics that need to be saved
-    // if there are any untracked traql entries
+    // if there are any untracked traql entries to be sent to the server
     if (Object.keys(traql).length > 1) {  //*-------------------------------------- TODO: WE HAVE TO MAKE THIS 2 BUT I THINK I NEED IT AT 1 FOR TESTING----------------*
+      let postReq = {
+        method: 'post',
+        url: 'http://localhost:3000/aqls'
+      };
       // loop through the untracked traql entries
       for (let key in traql) {
-        // if it's not the subResolver property and this mutation had any subscriptions (or subscribers) 
+        // if it's not the subResolver or UserToken property and this mutation had any subscriptions (or subscribers) 
         if (key !== 'subResolvers' && key !== 'userToken' && traql[key].expectedNumberOfAqls >= 1) {
           // if we receive back all of the expected Aqls, aka have resolved this traql entry
           if (traql[key].expectedNumberOfAqls === traql[key].aqlsReceivedBack.length) {
-            //TODO: THIS IS WHERE FIRST QUERY WILL BE SENT FROM
             // send the traql back to the server so it can be stored in the DB 
-            // add the key attribute to traql so the db has it
-            const postReq = {
-              method: 'post',
-              url: 'http://localhost:3000/aqls',
-              data: {
-                traql: traql[key],
-                entryType: 'success'
-              }
-            };
-            axios(postReq)
-
-            // LOOKS LIKE IT'S NOT SUCCESSFULLY DELETING TRAQL
-
-              .then(res => {
-                delete traql[key];
-                console.log('successful addition of success aqls to db');
-              })
-              .catch(err => console.log('err 1'));
+            aqlsToBeSent[successAqls].push(traql[key]);
+            delete traql[key];
           } else {
             // check if traql obj has "give me one more chance property"
             if (traql[key].probation) {
-              // if it doesnt, give it the property and continue
               // otherwise send successful aqls to server for entry into the db
-              // add the key attribute to traql so the db has it
-              const postReq = {
-                method: 'post',
-                url: 'http://localhost:3000/aqls',
-                data: {
-                  traql: traql[key],
-                  entryType: 'error'
-                }
-              };
-              axios(postReq)
-                .then(res => {
-                  delete traql[key];
-                  console.log('successful addition of error aqls to db');
-                })
-                .catch(err => console.log('err 2'));
+              aqlsToBeSent[errorAqls].push(traql[key]);
+              delete traql[key];
             } else {
               traql[key].probation = true;
             }
-          }      
+          }
         }          
-      }        
+      }   
+      postReq.data = aqlsToBeSent;  
+      axios(postReq)
+      .then(res => {
+        console.log('new traql, ', traql);
+        console.log('successful addition of aqls to db');
+        aqlsToBeSent = [];
+      })
+      .catch(err => console.log('err'));    
     }
     open = true;
   }
